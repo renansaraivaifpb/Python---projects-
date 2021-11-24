0ba8972714399b62271adb5a32a9263ae84e7c81
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2021-11-24 15:58:53 -0300

    Update nota_aluno.py

--- nota_aluno.py
+++ nota_aluno.py
@@ -1,6 +1,6 @@
-#faça um programa em python que o usuário insirá duas notas e calcule se ele passou ou não
-#EDIT (24/11/2021): palavra reservada do python try e except permite verificar se as notas são realmente possiveís de convertelas para tipo float, caso contrário, possivelmente, 
-não foi inserido numeros no input que tem como tipo string, logo, o except fecha o programa. 
+# faça um programa em python que o usuário insirá duas notas e calcule se ele passou ou não
+# EDIT (24/11/2021): palavra reservada do python try e except permite verificar se as notas são realmente possiveís de convertelas para tipo float, caso contrário, possivelmente, 
+# não foi inserido numeros no input que tem como tipo string, logo, o except fecha o programa. 
 nota_1 = input('Digite sua primeira nota: ')
 nota_2 = input('Digite sua segunda nota: ')
 try:
