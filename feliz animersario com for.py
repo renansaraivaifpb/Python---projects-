ab7d2f0353719222dd1816ebd2b28aae88b1dd4d
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2021-11-25 07:58:33 -0300

    Create feliz animersario com for.py

---
+++ feliz animersario com for.py
@@ -0,0 +1,10 @@
+# feliz animersario para nomes de uma váriavel através de for.
+
+mes = input("mês: ")
+if mes == 'dezembro':
+    dezembro = ['styles', 'ted', 'scot', 'derik']
+    for dezembro in dezembro:
+        print(f"Feliz aniversário, {dezembro}")
+    print('e é isso.')
+else:
+    print('ninguém registrado nesse mês.')
