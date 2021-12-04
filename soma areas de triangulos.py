7a3adc8f87c2c4b09887ecb57c4da2aab905566e
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2021-12-04 19:30:39 -0300

    Update soma areas de triangulos.py

--- soma areas de triangulos.py
+++ soma areas de triangulos.py
@@ -1,11 +1,10 @@
-
+area_total = 0
 def somar_areas_triangulos (comprimento, altura):
-  return (comprimento*altura)/2
+    return (comprimento*altura)/2
 for i in [1, 2, 3, 4]:
-  area_total = 0
-  c = float(input("Digite comprimento (m): ")
-  a = float(input("Digite altura (m): ")
-  triangulo = somar_areas_triangulos (c,a)
-  print(f"A área do triangulo {i} é: {triangulo} m²")
-  area_total = area_total + triangulo
+    c = float(input("Digite comprimento (m): "))
+    a = float(input("Digite altura (m): "))
+    triangulo = somar_areas_triangulos (c,a)
+    print(f"A área do triangulo {i} é: {triangulo} m²")
+    area_total = area_total + triangulo
 print(f"A soma das áreas dos quatros triângulos é: {area_total} m²")
